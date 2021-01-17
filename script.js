118cde9db5f48353da39448f04c055466424ac23
Grant Emerson <grant.alex.emerson@gmail.com>
2021-01-17 13:57:54 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const body = document.querySelector("body");
+const section1 = document.querySelector('.section1')
+const openDoor = './openDoor.svg';
+const closedDoor = './closedDoorBlack.svg';
+const words = document.querySelector('.paragraph');
+
+let doorKnocks = 1;
+const doorKnockSound = new Audio('./doorKnock.mp3');
+const doorOpenSound = new Audio('./doorOpen.mp3');
+const womenLaugh = new Audio("./PEOP_091.mp3");
+let isDoorOpen = false;
+
+document.querySelector('.section1').addEventListener('click', function () {
+    doorKnockSound.play();
+    console.log(`door has been knocked ${doorKnocks} times`);
+    if (doorKnocks === 3) {
+        isDoorOpen = true;
+        body.style.backgroundColor = "black";
+        doorOpenSound.play();
+        body.style.color = "black";
+        section1.style.backgroundColor = "black";
+        section1.style.backgroundImage = 'url(./openDoorWhite.svg)';
+        section1.style.height = '400px';
+        doorKnocks++;
+        words.innerHTML = "Enter If You Dare...";
+        words.style.color = "white";
+
+    } else if (doorKnocks === 1) {
+        doorKnocks++;
+        doorKnockSound.play();
+        words.innerHTML = "Who is it?"
+        isDoorOpen = false;
+
+    } else if (doorKnocks === 2) {
+        doorKnocks++;
+        words.innerHTML = "Leave me be! I do not like being bothered."
+        isDoorOpen = false;
+        doorKnockSound.play();
+
+    } else if (doorKnocks > 3 ) {
+        womenLaugh.play();
+    }
+})
